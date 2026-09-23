3af320e0163f5b112df1028df50a1e6382d3e4a1
Iris <a00843705@tec.mx>
2026-09-23 07:46:08 -0600

    Created the function monthToNumber so it is easy to sort, it works thanks to map

--- main.cpp
+++ main.cpp
@@ -2,20 +2,32 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <map>
 using namespace std;
 
 class Entry{
     private:
-    string month;
+        string month;
+        int day;
+        int hour;
+        string ip;
+        string reason;
+
     public:
-    void sorting(){
+    int monthToNumber(string month){
+        map<string, int> months{
+            {"Jun", 6},{"Jul", 7},{"Aug", 8},{"Sep", 9},{"Oct", 10}
+        };
+        return months[month];
     }
 };
 
 int main(){
     string myLine = "";
     ifstream readFile("bitacora.txt");
+    Entry testCase;
 
+    cout << testCase.monthToNumber("Jul");
 
     readFile.close();
     return 0;
