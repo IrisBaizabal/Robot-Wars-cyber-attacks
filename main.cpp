d8b0f565ff0dfd20f8f9e5134b2147060823c3ec
Iris <a00843705@tec.mx>
2026-09-22 20:59:13 -0600

    Basic structure just to have something to start with

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+class Entry{
+    private:
+    string month;
+    public:
+    void sorting(){
+    }
+};
+
+int main(){
+    string myLine = "";
+    ifstream readFile("bitacora.txt");
+
+
+    readFile.close();
+    return 0;
+}
